66a2c7cde72b03564f3f828a2bca6f66fcfee9ac
DenizAhmadi <ahmadideniz@gmail.com>
2023-04-06 18:06:14 +0430

    feat: create custom ProductCard

---
+++ kadoo/src/Pages/TestCard/TestCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomCard  from '../../Components/CustomCard/ProductCard';
+
+const TestCard = () => {
+  return (
+    <div>
+      <CustomCard />
+    </div>
+  );
+};
+
+export default TestCard;
