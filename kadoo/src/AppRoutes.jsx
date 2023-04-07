0d34b7cb5ceb1300bafe1f3f9b4dd57cb31b97b0
Navid.MSZD <s.navidm1380@gmail.com>
2023-04-07 12:05:21 +0430

    feat: main layout test

--- kadoo/src/AppRoutes.jsx
+++ kadoo/src/AppRoutes.jsx
@@ -24,34 +24,44 @@ import TicketPage from './Pages/TicketPage';
 import WriteTicket from './Components/WriteTicket';
 import TicketUser from './Pages/TicketUser';
 import Test from './Test'
+import MainLayout from './Pages/MainLayout';
 
 function AppRoutes() {  
   return (
-      <Routes>
-        <Route   path='/testredux' exact element={<Test/>}></Route>
-        <Route   path='/TicketPage' exact element={<TicketPage/>} />
-        <Route   path='/TicketUser' exact element={<TicketUser/>} />
-        <Route   path='/WriteTicket' exact element={<WriteTicket/>} />
-        <Route   path='/WateringUpdateTest' exact element={<WateringUpdate/>} />
-        <Route   path='/Coins' exact element={<ShowCoins/>} />
-        <Route   path='/' exact element={<LandingPage/>} />
-        <Route   path='/signin' exact element={<SignIn/>} />
-        <Route   path='/signup' exact element={<SignUp/>} />
-        <Route   path='/search/:text' exact element={<SearchResultProduct/>} />
-        <Route   path='/search/' exact element={<SearchResultProduct/>} />
-        <Route   path='/Homepage' exact element={<HomePage/>} />
-        <Route   path='/cart' exact  element={<Cart/>} />
-        <Route   path='/categories' exact  element={<CategoriesPage/>} />
-        <Route   path='/Reminder' exact  element={<Reminder/>} />
-        <Route   path='/greenhouse' exact  element={<Plantmanagement/>} />
-        <Route   path='/AdminPage' exact element={<AdminPage/>} />
-        <Route   path='/ProductPlantsPage/:id' exact element={<ProductPlantsPage/>}/>
-        <Route   path='/ProductToolsPage/:id' exact element={<ProductToolsPage/>} />
-        <Route   path='/greenHouseEdit/:id' exact element={<GreenHouseEdit/>} />
-        <Route   path='/greenHouseCreate/' exact element={<GreenHouseEdit/>} />
-        <Route  exact element={<NotFound/>} />
-      </Routes>
-  );
+    <Routes>
+      <Route path='/testredux' exact element={<Test />}></Route>
+      <Route path='/TicketPage' exact element={<TicketPage />} />
+      <Route path='/TicketUser' exact element={<TicketUser />} />
+      <Route path='/WriteTicket' exact element={<WriteTicket />} />
+      <Route path='/WateringUpdateTest' exact element={<WateringUpdate />} />
+      <Route path='/Coins' exact element={<ShowCoins />} />
+      <Route path='/' exact element={<LandingPage />} />
+      <Route path='/signin' exact element={<SignIn />} />
+      <Route path='/signup' exact element={<SignUp />} />
+      <Route path='/search/:text' exact element={<SearchResultProduct />} />
+      <Route path='/search/' exact element={<SearchResultProduct />} />
+      <Route path='/Homepage' exact element={<HomePage />} />
+      <Route path='/cart' exact element={<Cart />} />
+      <Route path='/categories' exact element={<CategoriesPage />} />
+      <Route path='/Reminder' exact element={<Reminder />} />
+      <Route path='/greenhouse' exact element={<Plantmanagement />} />
+      <Route path='/AdminPage' exact element={<AdminPage />} />
+      <Route
+        path='/ProductPlantsPage/:id'
+        exact
+        element={<ProductPlantsPage />}
+      />
+      <Route
+        path='/ProductToolsPage/:id'
+        exact
+        element={<ProductToolsPage />}
+      />
+      <Route path='/greenHouseEdit/:id' exact element={<GreenHouseEdit />} />
+      <Route path='/greenHouseCreate/' exact element={<GreenHouseEdit />} />
+      <Route path='/test-layout/test1/test2' element={<MainLayout />} />
+      <Route exact element={<NotFound />} />
+    </Routes>
+  )
 }  
 export default AppRoutes;
   
