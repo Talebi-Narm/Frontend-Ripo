70bc02221fd0dd1a851b86c5ab2c5304a088f210
nm4550 <nmashayekhi30@yahoo.com>
2023-04-20 15:20:18 +0330

    feat : favicon-and-title added

--- kadoo/src/App.jsx
+++ kadoo/src/App.jsx
@@ -1,33 +1,39 @@
 // Import Initial Files
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 // Import Theme Files
-import { ThemeProvider } from '@mui/material/styles'
-import Theme from './Theme/ThemeGenerator'
+import { ThemeProvider } from "@mui/material/styles";
+import Theme from "./Theme/ThemeGenerator";
 // Import Dom And React Components
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import CssBaseline from '@mui/material/CssBaseline'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Helmet from "react-helmet";
 // Import Pages
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
-import AppRoutes from './AppRoutes'
-import MainLayout from './Pages/MainLayout/index'
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AppRoutes from "./AppRoutes";
+import MainLayout from "./Pages/MainLayout/index";
 
 function App() {
   return (
-    <React.Fragment className='no-select'>
-      <ThemeProvider theme={Theme}>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <Router>
-            <Routes>
-              <Route path="/*" element={<AppRoutes />} />
-            </Routes>
-          </Router>
-        </LocalizationProvider>
-      </ThemeProvider>
-    </React.Fragment>
-  )
+    <>
+      <Helmet>
+        <title>Talebi</title>
+      </Helmet>
+      <React.Fragment className="no-select">
+        <ThemeProvider theme={Theme}>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Router>
+              <Routes>
+                <Route path="/*" element={<AppRoutes />} />
+              </Routes>
+            </Router>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </React.Fragment>
+    </>
+  );
 }
 
-export default App
+export default App;
