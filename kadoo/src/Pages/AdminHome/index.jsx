2281e4f0c19cb0015c0c3c0b77ac409699240774
Navid.MSZD <s.navidm1380@gmail.com>
2023-04-22 12:12:53 +0430

    fix: many ESLint errors 9

--- kadoo/src/Pages/AdminHome/index.jsx
+++ kadoo/src/Pages/AdminHome/index.jsx
@@ -1,18 +1,25 @@
+import React from "react";
+
 import Chart from "../../Components/chart";
 import FeaturedInfo from "../../Components/featuredInfo";
 import "./style.scss";
-import { userData } from "../../Constant/dummyData";
-import WidgetSm from "../../Components/widgetSm";
 import WidgetLg from "../../Components/widgetLg";
+import WidgetSm from "../../Components/widgetSm";
+import { userData } from "../../Constant/dummyData";
 
 export default function AdminHome() {
   return (
     <div className="homeAdmin">
-      <FeaturedInfo/>
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
