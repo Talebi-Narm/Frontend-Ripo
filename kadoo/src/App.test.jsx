dccfe770ef123b1759de71d42a4174ac2aafc0c9
navid-mousavizdeh <s.navidm1380@gmail.com>
2023-05-06 06:09:03 -0700

    feat: add test

--- kadoo/src/App.test.jsx
+++ kadoo/src/App.test.jsx
@@ -1,14 +1,41 @@
-import { render, waitFor } from "@testing-library/react";
-import * as React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import OfferPage from "./Pages/OfferPage";
+import axiosInstance from "./Utils/axios";
 
-describe("App", () => {
-  it("renders App component", async () => {
-    const { getByText } = render(<OfferPage />);
-    await waitFor(() => {
-      expect(getByText("code")).toBeInTheDocument();
+jest.mock("./Utils/axios");
+
+describe("OfferPage", () => {
+  beforeEach(() => {
+    axiosInstance.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: "50% off",
+          start_date: "2022-05-01",
+          end_date: "2022-05-31",
+          discount: 50,
+          code: "ABC123",
+        },
+      ],
     });
-    expect(getByText("l7aw49")).toBeInTheDocument();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("renders the offer card after fetching data from the API", async () => {
+    // eslint-disable-next-line react/react-in-jsx-scope
+    render(<OfferPage />);
+    expect(screen.getByRole("progressbar")).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(axiosInstance.get).toHaveBeenCalledWith("/v1/order/coupons");
+    });
+
+    expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
+    expect(screen.getByText("50% off")).toBeInTheDocument();
+    expect(screen.getByText("ABC123")).toBeInTheDocument();
   });
 });
