1482f904813407b4221256c16711cf3a9f17031d
navid-mousavizdeh <s.navidm1380@gmail.com>
2023-06-10 00:08:55 -0700

    feat: third depth comment and reply

--- kadoo/src/Components/Comment/index.jsx
+++ kadoo/src/Components/Comment/index.jsx
@@ -1,3 +1,4 @@
+import CloseIcon from "@mui/icons-material/Close";
 import ReplyIcon from "@mui/icons-material/Reply";
 import {
   Box,
@@ -15,6 +16,7 @@ import axiosInstance from "../../Utils/axios";
 
 function Comments({ id, tool }) {
   const [comments, setComments] = useState([]);
+  const [replyComment, setReplyComment] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
   const [newComment, setNewComment] = useState("");
   const [rating, setRating] = useState(1);
@@ -27,6 +29,26 @@ function Comments({ id, tool }) {
           ? { ...x, selected: !x.selected }
           : { ...x, selected: false }
       )
+    );
+    setReplyComment(
+      replyComment ? (replyComment.id === comment.id ? null : comment) : comment
+    );
+  };
+
+  const handleReplySecond = (comment) => {
+    setComments(
+      comments.map((x) => ({
+        ...x,
+        selected: false,
+        replies: x.replies.map((y) =>
+          y.id === comment.id
+            ? { ...y, selected: !y.selected }
+            : { ...y, selected: false }
+        ),
+      }))
+    );
+    setReplyComment(
+      replyComment ? (replyComment.id === comment.id ? null : comment) : comment
     );
   };
 
@@ -48,7 +70,17 @@ function Comments({ id, tool }) {
               profileImg: "",
               username: x.owner_detail.username,
               selected: false,
-              replies: response.data.filter((y) => y.reply_to === x.id),
+              replies: response.data
+                .filter((y) => y.reply_to === x.id)
+                .map((z) => ({
+                  id: z.id,
+                  text: z.text,
+                  rating: z.rate,
+                  profileImg: "",
+                  username: z.owner_detail.username,
+                  selected: false,
+                  replies: response.data.filter((k) => k.reply_to === z.id),
+                })),
             }))
         );
       })
@@ -74,13 +106,13 @@ function Comments({ id, tool }) {
       setIsSubmitting(true);
       let newCommentObj = {};
       if (tool) {
-        if (comments.find((x) => x.selected)) {
+        if (replyComment) {
           newCommentObj = {
             text: newComment,
             owner: userInfo.id,
             tool: id,
             rate: rating,
-            reply_to: comments.find((x) => x.selected).id,
+            reply_to: replyComment.id,
           };
         } else {
           newCommentObj = {
@@ -90,13 +122,13 @@ function Comments({ id, tool }) {
             rate: rating,
           };
         }
-      } else if (comments.find((x) => x.selected)) {
+      } else if (replyComment) {
         newCommentObj = {
           text: newComment,
           owner: userInfo.id,
           plant: id,
           rate: rating,
-          reply_to: comments.find((x) => x.selected).id,
+          reply_to: replyComment.id,
         };
       } else {
         newCommentObj = {
@@ -119,6 +151,7 @@ function Comments({ id, tool }) {
             setRating(1);
             fetchComments();
             setIsSubmitting(false);
+            setReplyComment(null);
           }
         })
         .catch((error) => {
@@ -178,6 +211,77 @@ function Comments({ id, tool }) {
               </IconButton>
             </Box>
           </Box>
+
+          {comment.replies.map((x) => (
+            <Box
+              sx={{
+                width: "100%",
+                backgroundColor: x.selected ? "whitesmoke" : "transparent",
+                borderRadius: "12px",
+                pl: 2,
+                pt: 0,
+                pb: 0,
+              }}
+            >
+              <Box
+                key={x.id}
+                display="flex"
+                alignItems="center"
+                sx={{
+                  opacity: isSubmitting ? 0.5 : 1,
+                  transition: "opacity 0.3s ease",
+                  pb: 2,
+                }}
+              >
+                <Box ml={2} sx={{ height: "100%" }} />
+                <Avatar src={x.profileImg} alt={x.username} />
+                <Box ml={2} sx={{ flexGrow: 1 }}>
+                  <Typography variant="caption" component="div">
+                    {x.username}
+                  </Typography>
+                  <Rating value={x.rating} readOnly />
+                  <Typography variant="body1">{x.text}</Typography>
+                </Box>
+                <Box sx={{ alignSelf: "flex-end" }}>
+                  <IconButton>
+                    <ReplyIcon onClick={() => handleReplySecond(x)} small />
+                  </IconButton>
+                </Box>
+              </Box>
+              {x.replies.map((y) => (
+                <Box
+                  sx={{
+                    width: "100%",
+                    borderRadius: "12px",
+                    pl: 2,
+                    pt: 0,
+                    pb: 0,
+                  }}
+                >
+                  <Box
+                    key={y.id}
+                    display="flex"
+                    alignItems="center"
+                    sx={{
+                      opacity: isSubmitting ? 0.5 : 1,
+                      transition: "opacity 0.3s ease",
+                      pb: 2,
+                    }}
+                  >
+                    <Box ml={4} sx={{ height: "100%" }} />
+                    <Avatar src={y.profileImg} alt={y.owner_detail.username} />
+                    <Box ml={2} sx={{ flexGrow: 1 }}>
+                      <Typography variant="caption" component="div">
+                        {y.owner_detail.username}
+                      </Typography>
+                      <Rating value={y.rate} readOnly />
+                      <Typography variant="body1">{y.text}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+          ))}
           <Box ml={2} sx={{ width: "100%" }}>
             <Divider sx={{ flexGrow: 1, mr: 4 }} />
           </Box>
@@ -187,31 +291,33 @@ function Comments({ id, tool }) {
       <Typography variant="body1" sx={{ mt: 2 }}>
         Write a Comment
       </Typography>
-      {comments.find((x) => x.selected) &&
-        comments
-          .filter((x) => x.selected)
-          .map((comment) => (
-            <Box
-              key={comment.id}
-              display="flex"
-              alignItems="center"
-              sx={{
-                opacity: 0.5,
-                transition: "opacity 0.3s ease",
-                pt: 1,
-                pb: 0,
-              }}
-            >
-              <Avatar src={comment.profileImg} alt={comment.username} />
-              <Box ml={2} sx={{ flexGrow: 1 }}>
-                <Typography variant="caption" component="div">
-                  {comment.username}
-                </Typography>
-                <Rating value={comment.rating} readOnly />
-                <Typography variant="body1">{comment.text}</Typography>
-              </Box>
-            </Box>
-          ))}
+      {replyComment && (
+        <Box
+          key={replyComment.id}
+          display="flex"
+          alignItems="center"
+          sx={{
+            opacity: 0.5,
+            transition: "opacity 0.3s ease",
+            pt: 1,
+            pb: 0,
+          }}
+        >
+          <Avatar src={replyComment.profileImg} alt={replyComment.username} />
+          <Box ml={2} sx={{ flexGrow: 1 }}>
+            <Typography variant="caption" component="div">
+              {replyComment.username}
+            </Typography>
+            <Rating value={replyComment.rating} readOnly />
+            <Typography variant="body1">{replyComment.text}</Typography>
+          </Box>
+          <Box sx={{ alignSelf: "flex-start" }}>
+            <IconButton>
+              <CloseIcon onClick={() => setReplyComment(null)} small />
+            </IconButton>
+          </Box>
+        </Box>
+      )}
 
       <Box display="flex" alignItems="center" mt={1} mb={2}>
         <Box sx={{ width: "100%", pr: 2 }}>
