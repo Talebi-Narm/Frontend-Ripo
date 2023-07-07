76cf56083ea2be786be7a31dbbda215a41ec8513
navid-mousavizdeh <s.navidm1380@gmail.com>
2023-07-07 01:29:42 -0700

    fix: handel 401

--- kadoo/src/Utils/axios.js
+++ kadoo/src/Utils/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
+let showToast = false;
 export const baseURL = "https://service.talebi-narm.ir/api/";
 const axiosInstance = axios.create({
   baseURL,
@@ -18,63 +20,28 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    const originalRequest = error.config;
+    console.log("errrrrorrr", error.response.data);
 
     if (typeof error.response === "undefined") {
-      alert(
-        "A server/network error occurred. " +
+      console.log(
+        "A server/network error occurred. handelt here " +
           "Looks like CORS might be the problem. " +
           "Sorry about this - we will get it fixed shortly."
       );
       return Promise.reject(error);
     }
-
-    if (
-      error.response.status === 401 &&
-      originalRequest.url === `${baseURL}token/refresh/`
-    ) {
-      window.location.href = "/login/";
-      return Promise.reject(error);
-    }
-    if (
-      error.response.status === 401 &&
-      error.response.data.code === "token_not_valid"
-    ) {
+    if (error.response.status === 401) {
       // Redirect to login page if user is not logged in
-      window.location.href = "/login/";
-      return Promise.reject(error);
-    }
-
-    if (
-      error.response.data.code === "token_not_valid" &&
-      error.response.status === 401 &&
-      error.response.statusText === "Unauthorized"
-    ) {
-      const refreshToken = localStorage.getItem("refresh_token");
-
-      if (refreshToken) {
-        const tokenParts = JSON.parse(atob(refreshToken.split(".")[1]));
-        const now = Math.ceil(Date.now() / 1000);
-        if (tokenParts.exp > now) {
-          return axiosInstance
-            .post("/token/refresh/", { refresh: refreshToken })
-            .then((response) => {
-              localStorage.setItem("access_token", response.data.access);
-              localStorage.setItem("refresh_token", response.data.refresh);
-
-              axiosInstance.defaults.headers.Authorization = `JWT ${response.data.access}`;
-              originalRequest.headers.Authorization = `JWT ${response.data.access}`;
-
-              return axiosInstance(originalRequest);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
-        window.location.href = "/login/";
-      } else {
-        window.location.href = "/login/";
+      if (!showToast) {
+        toast.error("You're not loged in :( you're redirecting to login page.");
+        window.localStorage.removeItem("access_token");
+        window.localStorage.removeItem("refresh_token");
+        showToast = true;
       }
+      setTimeout(() => {
+        window.location.href = "/signin";
+        return Promise.reject(error);
+      }, 5000);
     }
 
     return Promise.reject(error);
